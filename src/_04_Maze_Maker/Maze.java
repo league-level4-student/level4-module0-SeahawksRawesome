4f8658bee025b9e0eeeb7a443b8fbd251548b6ba
SeahawksRawesome <league@WTS-IM-6.attlocal.net>
2019-04-20 11:21:55 -0700

    On the last part then mod 1!
    :smile:

--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 
 public class Maze {
 	//1. Create a 2D array of cells. Don't initialize it.
-
+	
 	private int width;
 	private int height;
 
@@ -11,7 +11,7 @@ public class Maze {
 		this.width = w;
 		this.height = h;
 
-		//2. Initialize the cells using the width and height varibles
+		//2. Initialize the cells using the width and height variables
 
 		//3. Iterated through each cell and initialize it
 		//   using i and j as the location
